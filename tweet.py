b74a45ce1a9a4bb4bfd4417c91d9072304bb0f04
esraahusamia <esraa.hus93@yahoo.com>
2017-11-14 18:37:10 +0200

    tweet in db

--- tweet.py
+++ tweet.py
@@ -1,17 +1,11 @@
-from flask import Flask, render_template, redirect
+
 from twitter import *
 from tkinter import *
-import mysql.connector as mariadb
-app = Flask(__name__)
-
-mariadb_connection = mariadb.connect(user='root', password='123456', database='tweets')
-cursor = mariadb_connection.cursor()
 def showTweets(x, num):
      # display a number of new tweets and usernames 
      for i in range(0, num):
           line1 = (x[i]['user']['screen_name']) 
           line2 = (x[i]['text'])
-          cursor.execute("INSERT INTO tweet (user,text) VALUES (%s,%s)", (line1,line2))
           w = Label(master, text=line1 + "\n" + line2 + "\n\n") 
           w.pack()
 
@@ -29,8 +23,11 @@ def getTweets():
              entryWidget.delete(0,END)
              print("working")
 
+
+# Put in token, token_key, con_secret, con_secret_key
 t = Twitter( auth=OAuth('929748215058042883-eVIkYLyUIe6Uzm3gKiaXMTCSrQMUMNd', 'TaXvz7PhV9cOcrolPtcy4aBhXrpuXc0jjdG0Urouvz4Zo', 'aipWkJx95L2yxLw1Ud7tJggaY', '8e9LBVAuLDrULNanjxs5UQj4DMRnQyngd5ERxWIoALZlJos3vu'))
-numberOfTweets =20
+
+numberOfTweets = 20
 
 master = Tk()
 showTweets(getTweets(), numberOfTweets)
@@ -51,17 +48,5 @@ entryWidget.pack(side=LEFT)
 textFrame.pack()
 
 
+master.mainloop()
 
-
-@app.route('/')
-def index():
-     return "hi"
-
-@app.route('/home')
-def home():
-    tweets = cursor.execute("SELECT * FROM tweet")
-        return render_template('home.html',tweets= tweets)
-
-if __name__ == '__main__':
-    master.mainloop()
-    app.run(debug=True)
